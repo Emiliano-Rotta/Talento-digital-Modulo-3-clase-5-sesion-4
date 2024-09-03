fc1de19fb7b77242fd1c7e7bb51c109de2a6b100
Emiliano Rotta <emilianorottaf@gmail.com>
2024-09-02 21:48:22 -0300

    ejercicio grupal

---
+++ assets/js/ejercicioGrupal.js
@@ -0,0 +1,18 @@
+//Preferentemente usar oprador ternario
+
+//Ejercicio 1: Validación de Formularios
+// Consigna:
+// Crea una función validar que reciba tres parámetros: usuario, contraseña, y anio. La función debe validar lo siguiente:
+// El usuario no debe estar vacío.
+// la contraseña debe tener al menos 8 caracteres.
+// anio debe ser mayor o igual a 18.
+
+// Si todas las validaciones son correctas, la función debe devolver 'Formulario válido'. Si alguna falla, debe devolver un mensaje indicando cuál validación falló. Usa operadores lógicos y ternarios.
+
+// Ejercicio 2: Determinación de Horario de Atención
+// Consigna:
+// Crea una función estaAbierto que reciba dos parámetros: dia y hora. La función debe determinar si una tienda está abierta basado en los siguientes horarios:
+// La tienda está abierta de lunes a viernes de 9 AM a 6 PM.
+// Los sábados, está abierta de 10 AM a 4 PM.
+// Los domingos está cerrada.
+// La función debe devolver 'Abierto' o 'Cerrado' utilizando operadores lógicos y ternarios.
